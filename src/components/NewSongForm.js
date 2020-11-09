0a237611a99758ab6860050bb9ac40b725b9c62e
omid esmaeili <omidesmaeeli4@gmail.com>
2020-11-09 14:13:27 +0330

    Concept using "useState" and "useEffect"

---
+++ src/components/NewSongForm.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+const NewSongForm = ({addSong}) => {
+    const [title, setTitle] = useState("") // Initial value is empty string
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        addSong(title) // Get function for call of parent component
+        setTitle("")
+    }
+
+    return (
+        <form onSubmit={handleSubmit}> {/* Call after click submit */}
+            <label htmlFor="name">Song name:</label>
+            <input type="text" value={title} required
+                   onChange={(e) => setTitle(e.target.value)}/> {/* onChanged call every change value (Enter key value) */}
+            <input type="submit" value="add song"/>
+        </form>
+    )
+}
+
+export default NewSongForm
---
+++ src/components/SongList.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react';
+import {v4 as uuidv4} from 'uuid';
+import NewSongForm from "./NewSongForm";
+
+const SongList = () => {
+    // Concept using "useState" and "useEffect"
+
+    const [songs, setSongs] = useState([
+        {title: 'song 1', id: 1},
+        {title: 'song 2', id: 2},
+        {title: 'song 3', id: 3}
+    ]) // Initial value is list of dictionary songs
+
+    const [age, setAge] = useState(24) // Initial value is 24
+
+
+    const addSong = (title) => {
+        return setSongs([...songs, {title, id: uuidv4()}])
+    } // Add new song to songs state variable
+
+    useEffect(() => {
+        console.log("useEffect hook ran without inputs")
+    }) // Call every time update page or refresh page or render page or re-render page
+
+    useEffect(() => {
+        console.log("useEffect hook ran", songs)
+    }, [songs]) // Call after change songs
+
+    useEffect(() => {
+        console.log("useEffect hook ran", age)
+    }, [age]) // Call after change age
+
+    useEffect(() => {
+        console.log("useEffect hook ran []")
+    }, []) // Call after page refresh
+
+
+    return (
+        <div className="song-list">
+            <h3>Concept using "useState" and "useEffect"</h3>
+            <ul>
+                {songs.map(song => {
+                    return (
+                        <li key={song.id}>{song.title}</li>
+                    )
+                })}
+            </ul>
+
+            <NewSongForm addSong={addSong}/>
+            <br/>
+            <button onClick={() => setAge(age + 1)}>Your age is : {age}</button>
+        </div>
+    );
+};
+
+export default SongList
