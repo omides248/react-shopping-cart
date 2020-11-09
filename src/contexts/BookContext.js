c6d237362f4aaca38434d7ea6a5162c9c55cc578
omid esmaeili <omidesmaeeli4@gmail.com>
2020-11-09 21:10:20 +0330

    Using localStorage - Add All books to localStorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,11 +1,19 @@
-import React, {createContext, useReducer} from 'react';
-import {v4 as uuidv4} from "uuid"
+import React, {createContext, useReducer, useEffect} from 'react';
 import {bookReducer} from "../reducers/BookReducer";
 
 export const BookContext = createContext()
 
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer, [])
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        // If page refresh localStorage is []
+        // For fix this problem get date of localStorage before refresh page and set for initial into localStorage
+        const localData = localStorage.getItem("books")
+        return localData ? JSON.parse(localData) : []
+    })
+
+    useEffect(() => {
+        localStorage.setItem("books", JSON.stringify(books))
+    }, [books]) // Every time change books set All books to localStorage
 
     return (
         <BookContext.Provider value={{books, dispatch}}>
