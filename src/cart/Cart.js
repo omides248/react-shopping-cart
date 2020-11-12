4f06291bb53672b4b640a5b13131b191c4dd399f
omid esmaeili <omidesmaeeli4@gmail.com>
2020-11-12 17:44:36 +0330

    Add increment and decrement and total for cart

--- src/cart/Cart.js
+++ src/cart/Cart.js
@@ -1,12 +1,12 @@
 import React, {useContext} from 'react';
 import './Cart.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faTrash, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
 import {CartContext} from "../contexts/CartContext";
 
 function Cart(props) {
 
-    const {cartProducts, removeProductToCart} = useContext(CartContext)
+    const {cartProducts, removeProductToCart, decrementProductOfCart, incrementProductToCart, totalCart} = useContext(CartContext)
     return (
         <div className="div-table">
             <table className="table-cart">
@@ -16,7 +16,8 @@ function Cart(props) {
                     <th>تصویر</th>
                     <th>نام کالا</th>
                     <th>تعداد</th>
-                    <th className="th-remove-tag">قیمت</th>
+                    <th>قیمت</th>
+                    <th className="th-remove-tag">قیمت کل</th>
 
                 </tr>
                 </thead>
@@ -28,14 +29,22 @@ function Cart(props) {
                             <td className="td-remove-tag-right"><FontAwesomeIcon icon={faTrash} className="trash-icon-cart" onClick={() => removeProductToCart(product.id)}/></td>
                             <td>image<img src="" height={50} width={50} alt=""/></td>
                             <td>{product.title}</td>
-                            <td>{product.quantity}</td>
-                            <td className="td-remove-tag-left">{product.price}</td>
+                            <td><FontAwesomeIcon icon={faMinus} onClick={() => decrementProductOfCart(product.id)}/> {product.quantity} <FontAwesomeIcon icon={faPlus} onClick={() => incrementProductToCart(product.id)}/></td>
+                            <td>{product.price}</td>
+                            <td className="td-remove-tag-left">{product.total || product.price}</td>
 
                         </tr>
                     )
                 })}
+                <tr>
+                    <td className="td-remove-tag-right"/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td/>
+                    <td className="td-remove-tag-left">{totalCart}</td>
 
-
+                </tr>
                 </tbody>
 
             </table>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -7,23 +7,27 @@ const CartContextProvider = (props) => {
 
     const [cartProducts, setProductsToCart] = useState([])
     const [countProducts, setCountProducts] = useState(0)
+    const [totalCart, setTotalCart] = useState(0)
 
 
     const addProductToCart = (id, title, quantity, price) => {
         let is_found_product = false
         console.log("cartProducts:", cartProducts.length, cartProducts)
         if (cartProducts.length === 0) {
-            setProductsToCart([...cartProducts, {id, title, quantity, price}])
+            setProductsToCart([...cartProducts, {id, title, quantity, price, total: price}])
             console.log("c1 first")
             is_found_product = true
             setCountProducts(countProducts + 1)
+            setTotalCart(price)
         } else {
             for (let i in cartProducts) {
                 console.log("Before : ", id, title, i)
                 if (cartProducts[i].id === id) {
                     cartProducts[i].quantity += 1
+                    cartProducts[i].total += price
                     setProductsToCart(cartProducts)
                     setCountProducts(countProducts + 1)
+                    setTotalCart(totalCart + cartProducts[i].price)
                     console.log("c2 exists")
                     is_found_product = true
                     break
@@ -32,8 +36,9 @@ const CartContextProvider = (props) => {
         }
 
         if (!is_found_product) {
-            setProductsToCart([...cartProducts, {id, title, quantity, price}])
+            setProductsToCart([...cartProducts, {id, title, quantity, price, total: price}])
             setCountProducts(countProducts + 1)
+            setTotalCart(totalCart + price)
             console.log("c3 new ")
         }
 
@@ -42,17 +47,50 @@ const CartContextProvider = (props) => {
     }
 
     const removeProductToCart = (id) => {
+        let q = 0
+        let t = 0
+        for (let i in cartProducts) {
+            if (cartProducts[i].id === id) {
+                q = cartProducts[i].quantity
+                t = cartProducts[i].price * q
+                break
+            }
+        }
+        setProductsToCart(cartProducts.filter(product => product.id !== id))
+        setCountProducts(countProducts - q)
+        setTotalCart(totalCart - t)
+    }
 
+    const incrementProductToCart = (id) => {
+        console.log("1Before : ", cartProducts)
+        for (let i in cartProducts) {
+            if (cartProducts[i].id === id) {
+                cartProducts[i].quantity += 1;
+                cartProducts[i].total += cartProducts[i].price;
+                setProductsToCart(cartProducts)
+                setCountProducts(countProducts + 1)
+                setTotalCart(totalCart + cartProducts[i].price)
+                break
+            }
+        }
+        console.log("1After : ", cartProducts)
+    }
+
+    const decrementProductOfCart = (id) => {
         for (let i in cartProducts) {
             if (cartProducts[i].id === id) {
                 let old_quantity = cartProducts[i].quantity;
                 if (old_quantity > 1) {
                     cartProducts[i].quantity -= 1;
+                    cartProducts[i].total -= cartProducts[i].price;
                     setProductsToCart(cartProducts)
                     setCountProducts(countProducts - 1)
+                    setTotalCart(totalCart - cartProducts[i].price)
                 } else {
                     setProductsToCart(cartProducts.filter(product => product.id !== id))
                     setCountProducts(countProducts - 1)
+                    setTotalCart(totalCart - cartProducts[i].price)
+                    cartProducts[i].total -= cartProducts[i].price;
                 }
             }
         }
@@ -60,7 +98,16 @@ const CartContextProvider = (props) => {
 
 
     return (
-        <CartContext.Provider value={{cartProducts, countProducts, addProductToCart, removeProductToCart}}>
+        <CartContext.Provider
+            value={{
+                cartProducts,
+                countProducts,
+                addProductToCart,
+                removeProductToCart,
+                decrementProductOfCart,
+                incrementProductToCart,
+                totalCart
+            }}>
             {props.children}
         </CartContext.Provider>
     )
