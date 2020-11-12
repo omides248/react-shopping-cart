2642fe1af5349c517045c1f4ac23a622a28fbbd2
omid esmaeili <omidesmaeeli4@gmail.com>
2020-11-12 23:03:10 +0330

    Add useFetch and fix css

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+const ip = "127.0.0.1"
+const port = "8000"
+const protocol = "http"
+const host = protocol + "://" + ip + ":" + port + "/api/"
+
+export default host;
--- src/products/components/ProductList.js
+++ src/products/components/ProductList.js
@@ -1,36 +1,47 @@
-import React, {useState, useContext} from 'react';
-import {v4 as uuidv4} from 'uuid';
+import React, {useState, useContext, useEffect} from 'react';
+// import {v4 as uuidv4} from 'uuid';
+import axios from 'axios'
+import host from '../../config/config'
 import './ProductList.css';
 import {CartContext} from "../../contexts/CartContext";
+import useFetch from "../../hooks/UseFetch"
+
 
 function ProductList() {
 
+    const {data, success} = useFetch("products")
+
     const {addProductToCart} = useContext(CartContext)
 
-    const products = [
-        {id: 1, title: "کفش ورزشی1", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 2, title: "کفش ورزشی2", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 3, title: "کفش ورزشی3", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 4, title: "کفش ورزشی4", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 5, title: "کفش ورزشی5", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 6, title: "کفش ورزشی6", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 7, title: "کفش ورزشی7", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-        {id: 8, title: "کفش ورزشی8", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
-    ]
+
+    // const products = [
+    //     {id: 1, title: "کفش ورزشی1", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 2, title: "کفش ورزشی2", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 3, title: "کفش ورزشی3", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 4, title: "کفش ورزشی4", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 5, title: "کفش ورزشی5", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 6, title: "کفش ورزشی6", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 7, title: "کفش ورزشی7", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    //     {id: 8, title: "کفش ورزشی8", price: 200, description: "این کفش با رنگ ها و سایز های مختلف موجود است"},
+    // ]
 
 
     return (
         <>
-            {products && products.map(product => {
+            {!success ? <div style={{textAlign: "center"}}>loading</div> : data && data.map(product => {
                 return (
                     <div className="column" key={product.id}>
                         <div className="card">
-                            <img src="" alt="Denim Jeans"/>
-                            <h3>{product.title}</h3>
+                            <img src={product.image} alt="Denim Jeans"
+                                 style={{width: "100px", height: "100px", objectFit: "contain"}}/>
+                            <h3>{product.name}</h3>
                             <p className="price">{product.price} تومان</p>
-                            <p>{product.description}</p>
+                            <p>{product.description ? product.description.substring(0, 10) : ""}</p>
                             <p>
-                                <button type="submit" onClick={ () => addProductToCart(product.id, product.title, 1, product.price)}>افزودن به سبد خرید</button>
+                                <button type="submit"
+                                        onClick={() => addProductToCart(product.id, product.title, 1, product.price)}>افزودن
+                                    به سبد خرید
+                                </button>
                             </p>
                         </div>
                     </div>
